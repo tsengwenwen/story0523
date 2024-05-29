19f842bf9afdf9a419bda4554e3e8f117ddb827a
tsengwenwen <msfn387908@gmail.com>
2024-05-29 20:07:51 +0800

    Initial Commit

---
+++ story0523/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  story0523
+//
+//  Created by TsengWen on 2024/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ story0523/story0523App.swift
@@ -0,0 +1,17 @@
+//
+//  story0523App.swift
+//  story0523
+//
+//  Created by TsengWen on 2024/5/29.
+//
+
+import SwiftUI
+
+@main
+struct story0523App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
